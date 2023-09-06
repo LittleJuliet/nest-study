9e4fa2ca6be11a8f20b406fb18d76ef4606eaf5c
juliet <luoxianliang@abcyun.cn>
2023-09-06 18:48:40 +0800

    feat: 装饰器2

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, Get, Ip, Query, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Ip,
+  Param,
+  Post,
+  Query,
+  Req,
+} from '@nestjs/common';
 import { Request } from 'express';
+import { CreateCatDto } from './create-cat.dto';
 
-@Controller('cats')
+@Controller({ path: 'cats' })
 export class CatsController {
   @Get('cats')
-  findAll(@Req() req: Request) {
-    console.log(req);
+  findAll() {
     return 'This action returns all cats.';
   }
 
@@ -17,5 +27,27 @@ export class CatsController {
   @Get('ip')
   getIp(@Ip() ip: string) {
     return `Your IP is ${ip}`;
+  }
+
+  @Post('error')
+  @HttpCode(200)
+  createError(@Req() req: Request) {
+    const { isError = 0 } = req.body;
+    if (isError) {
+      throw new Error('This is a error');
+    }
+    return 'This action return a error.';
+  }
+
+  @Get('get/:id')
+  getCatById(@Param() params) {
+    const { id } = params;
+    return `You get a cat which id is ${id}`;
+  }
+
+  @Post('create')
+  @HttpCode(200)
+  createCat(@Body() cat: CreateCatDto) {
+    return { ...cat };
   }
 }
