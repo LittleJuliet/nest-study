ad6af8c7df28b4cf176fa4147734f09afddfb5b0
juliet <luoxianliang@abcyun.cn>
2023-09-07 18:50:30 +0800

    feat: 添加cats.service

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,53 +1,18 @@
-import {
-  Body,
-  Controller,
-  Get,
-  HttpCode,
-  Ip,
-  Param,
-  Post,
-  Query,
-  Req,
-} from '@nestjs/common';
-import { Request } from 'express';
-import { CreateCatDto } from './create-cat.dto';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { CreateCatDto } from './dto/create-cat.dto';
 
-@Controller({ path: 'cats' })
+@Controller('cats')
 export class CatsController {
-  @Get('cats')
-  findAll() {
-    return 'This action returns all cats.';
-  }
-
-  @Get('header')
-  getHeader(@Query('name') name: string) {
-    return `The query name is ${name}`;
-  }
-
-  @Get('ip')
-  getIp(@Ip() ip: string) {
-    return `Your IP is ${ip}`;
-  }
-
-  @Post('error')
-  @HttpCode(200)
-  createError(@Req() req: Request) {
-    const { isError = 0 } = req.body;
-    if (isError) {
-      throw new Error('This is a error');
-    }
-    return 'This action return a error.';
-  }
-
-  @Get('get/:id')
-  getCatById(@Param() params) {
-    const { id } = params;
-    return `You get a cat which id is ${id}`;
-  }
+  constructor(private catsService: CatsService) {}
 
   @Post('create')
-  @HttpCode(200)
-  createCat(@Body() cat: CreateCatDto) {
-    return { ...cat };
+  create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
+  }
+
+  @Get('findAll')
+  findAll() {
+    return this.catsService.findAll();
   }
 }
